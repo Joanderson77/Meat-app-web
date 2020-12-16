d81ae4fad083a6f3b6ec87cc0ff11b07da004ecf
Mateus Joanderson <mateusjoanderson@gmail.com>
2020-12-16 15:54:43 -0300

    Forms tamplate impelementados

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { RestaurantsComponent } from './restaurants/restaurants.component';
 import { RestaurantDetailComponent}  from './restaurant-detail/restaurant-detail.component'
 import { MenuComponent } from './restaurant-detail/menu/menu.component';
 import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
+import { OrderComponent } from './order/order.component';
 
 
 export const ROUTES: Routes = [
@@ -15,10 +16,11 @@ export const ROUTES: Routes = [
 children:[
     {path:'', redirectTo: 'menu', pathMatch: 'full'},
     {path: 'menu', component:MenuComponent},
-    {path: 'reviews', component:ReviewsComponent}
+    {path: 'reviews', component:ReviewsComponent},
+    
 ]
 
 },
 
-
+{path: 'order', component:OrderComponent}
 ]
